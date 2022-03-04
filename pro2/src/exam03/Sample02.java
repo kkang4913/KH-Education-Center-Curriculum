d354681e0057929890f9da63c4a0eda221c3bca8
강승호 <kkang4913@gmail.com>
2022-03-04 21:46:51 +0900

    변수/ 상수

---
+++ pro2/src/exam03/Sample02.java
@@ -0,0 +1,25 @@
+package exam03;
+
+public class Sample02 {
+public static void main(String[] args) {
+	//지역변수는 반드시 초기화가 필요
+	int x;					//공간만 만들어주고 값이 없는데 
+	x=1;
+	System.out.println(x);
+	
+	/*상수 : 한 번 초기화를 한 후에는 값을 수정할 수 없다.
+	 *변수 선언 방식과 유사마려, 변수 선언 할 때 쓰이는 자료형 앞에 final 예약어를 사용
+	 *
+	 * 
+	 */
+	final int i;
+	i = 2;
+//	i = 3; // 값을 수정 할 수 없기 때문에 오류가 발생한다.
+	System.out.println(i);
+	String str = "기차" +123 + 45 + "출발";
+	System.out.println(str);
+	String str2 = 123+45 + "기차" + "칙칙폭폭";
+	System.out.println(str2);
+	
+}
+}
