bd9a47ec2c9ed3e5b0fd05ccb41225286c9edd5f
강승호 <kkang4913@gmail.com>
2022-03-11 16:53:49 +0900

    while / do...while문

---
+++ pro03/src/exam02/Sample08.java
@@ -0,0 +1,50 @@
+package exam02;
+
+public class Sample08 {
+public static void main(String[] args) {
+	/*
+	 * while문
+	 * 		while(조건식){
+	 * 			반복 수행할 코드 작성
+	 * 		}
+	 * for 문과 다르게 초기식, 증감식에 대한 형식이 없지만 일정 횟수 반복을 위해서는
+	 * for 문과 동일하게 초기식, 증감식에 대한 작성 필요하다.
+	 */
+
+	int i = 0;
+	while (i < 5) {
+		System.out.println("i ->" + i);
+		i++;
+	}
+	/*
+	 * do....while문
+	 * 		do{
+	 * 			반복 수행할 코드 작성
+	 * 		}while(조건식);  중요하다.
+	 * 일단 최초 1회는 무조건 실행
+	 * 2번째 실행부터는 조건식의 결과가 참일 때 실행이 된다.
+	 * 조건식이 참일 때 do 다시 실행
+	 */
+	i = 0; // 초기식
+	do {
+		System.out.println("i ->" + i);
+		i++; // 증감식
+	} while (i < 5);
+	/*
+	 * break 문
+	 * 		-반복문을 강제로 종료시키기 위해 사용하는 구문
+	 * continue 문
+	 * 		-반복문 안에서 다음 실행할 코드의 진행을 막고 다시 반복문의 처음으로 돌려보내주는 구문
+	 */
+	i = 0;
+	while (i < 10) {
+		i += 1;
+	if (i % 3 == 0) {
+		System.out.println("i -> " + i);
+	}else {
+		continue;
+	}
+	System.out.println("이 출력은 3의 배수일 때만 실행 됩니다.");
+	}
+}
+}
