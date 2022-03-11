861ea2d6167b044cb879f369c1a5cb3e976cc82e
강승호 <kkang4913@gmail.com>
2022-03-11 21:52:15 +0900

    동일한 문자열 값을 판별하는 방식

--- pro03/src/exam02/Sample09.java
+++ pro03/src/exam02/Sample09.java
@@ -1,5 +1,7 @@
 package exam02;
 
+import java.util.Scanner;
+
 public class Sample09 {
 
 	public static void main(String[] args) {
@@ -25,6 +27,28 @@ public class Sample09 {
 		s = "Hello java Programming";
 		int len = s.length();
 		System.out.println("문자열의 문자수 : " + len);
+		
+		/*
+		 *문자열과 문자열 비교하는 방법
+		 *	문자열 변수명.eqials(문자열) 
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("문자열을 입력하세요 : ");
+		String str = sc.nextLine();
+		String str1 ="문자열";
+		
+//		if (str1.equals(str)) {
+//			System.out.println("같습니다");
+//		}else {
+//			System.out.println("달라요");
+//		}
+		
+		if (str1==str) {
+			System.out.println("==로 비교한 문자열 값입니다.");
+		}else {
+			System.out.println("문자열 값이 다릅니다.");  //문자열이 지니고 있는 특징 중 하나 // 꼭 equals 적용
+		}
+		
 	}
 
 }
