4745e12c7d806e60c5ffe438c67130f95c880fae
강승호 <kkang4913@gmail.com>
2022-03-15 01:27:55 +0900

    출력 / 주석

--- pro01/src/exam01/Sample01.java
+++ pro01/src/exam01/Sample01.java
@@ -5,6 +5,13 @@ public class Sample01 {
 	public static void main(String[] args) {
 		//시스아웃 + 컨트롤 + 스페이스바 단축키 / 콘솔에 출력 
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 		
+		// 한 줄 주석 : 코드로 실행하지 않는 문장
+		/*
+		 * 여러 줄 주석 : 코드로 실행하지 않는 문장
+		 * 여러 줄에 대해서도 주석으로 처리해 준다.
+		 */
 	}
 }
