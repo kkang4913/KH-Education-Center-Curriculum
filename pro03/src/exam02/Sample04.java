ea99185cf3c796aeef1cc78681a8acf1c4e162f1
강승호 <kkang4913@gmail.com>
2022-03-14 16:44:00 +0900

    반복문

--- pro03/src/exam02/Sample04.java
+++ pro03/src/exam02/Sample04.java
@@ -11,8 +11,7 @@ public class Sample04 {
 
 		// 3번 입력 제한을 설정했음
 		// 3번 입력 제한 안에 올바른 값을 입력하지 않은 경우 -> 입력 횟수를 초과하였습니다.
-		int i;
-		for (i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.print("1 ~ 5 사이의 정수값 입력 : ");
 			num = sc.nextInt();
 
