8f2023a0d1e5fd9cccfae99d5a5f9cda3dde1dfa
강승호 <kkang4913@gmail.com>
2022-03-17 17:37:22 +0900

    객체

---
+++ pro05/src/exam01/Sample01.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Sample01 {
+public static void main(String[] args) {
+	//만들어진 Person을 Person타입의 p에 저장해라
+	//인스턴스화
+	Person p = new Person();  
+
+	p.pNo = "";
+
+	
+
+}
+}
