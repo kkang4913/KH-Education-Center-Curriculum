4f82a99e8aed58ecca774d7654dfe17b518719de
강승호 <kkang4913@gmail.com>
2022-03-17 21:48:56 +0900

    추상화하여 클래스 만들어 보기

---
+++ pro05/src/exam06/StopWatch.java
@@ -0,0 +1,31 @@
+package exam06;
+
+public class StopWatch {
+		private  int hour;
+		private  int min;
+		private  int sec;
+		
+		public void setHour(int h) {  //setter
+			hour =h;
+		}
+		public void getHour(int h) {  //setter
+			hour =h;
+		}
+		
+		
+		
+		public void start() {
+
+		}
+		public void stop() {
+
+		}
+		public void reset() {
+
+		}
+		public void pause() {
+
+		}
+}
+
+
