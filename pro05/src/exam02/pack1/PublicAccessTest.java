2deea912533d2ba3d8b8ba3bdbf850a942ded1cc
강승호 <kkang4913@gmail.com>
2022-03-17 17:37:14 +0900

    클래스에서 사용하는 접근제한자

---
+++ pro05/src/exam02/pack1/PublicAccessTest.java
@@ -0,0 +1,12 @@
+package exam02.pack1;
+// public은 반드시 하나, 여러개 만들면 오류 발생
+// 하나의 자바파일에는 하나의 public
+public class PublicAccessTest {
+
+}
+
+class DefaultAccessTest{
+	
+}
+
+
---
+++ pro05/src/exam02/pack2/Sample02.java
@@ -0,0 +1,13 @@
+package exam02.pack2;
+
+import exam02.pack1.PublicAccessTest;
+import exam02.pack1.DefaultAccessTest;
+
+public class Sample02 {
+
+	public static void main(String[] args) {
+		PublicAccessTest p = new PublicAccessTest();
+		DefaultAccessTest d = new DefaultAccessTest();
+	}
+
+}
