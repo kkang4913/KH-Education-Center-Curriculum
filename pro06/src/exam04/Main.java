102c83accac5fe7e4e9132145ab50b7d16776ec5
강승호 <kkang4913@gmail.com>
2022-03-25 19:33:54 +0900

    정렬 기능 분리 /추가

--- pro06/src/exam04/Main.java
+++ pro06/src/exam04/Main.java
@@ -1,5 +1,6 @@
 package exam04;
 
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,16 +11,27 @@ public class Main {
 		 * GradeList 에는 Grade 를 배열로 관리하는 객체 -과목에 대한 성적 관리를 위해 Grade 객체를 추가/ 수정/ 삭제/ 조회
 		 * 기능을 만든다. -가능하면 가장 높은 점수를 가지는 과목으로 정렬 할 수 있는 기능을 만든다.
 		 */
-		
+
 		GradeList datas = new GradeList();
-		datas.add(new Grade("국어",67.8));
-		System.out.println("국어" + datas.getScore("국어"));
-		
-		
-		Grade g = datas.getGrade(0);
-		g.setName("영어");	g.setScore(78.7);
-		
-		System.out.println("영어" + g.getScore());
-		
+		datas.add(new Grade("영어", 67.8));
+		datas.add(new Grade("국어", 37.8));
+		datas.add(new Grade("수학", 90.8));
+		datas.add(new Grade("과학", 39.8));
+		datas.add(new Grade("사회", 70.8));
+
+		System.out.println(Arrays.toString(datas.getUnder()));
+		System.out.println(Arrays.toString(datas.getUnder(90)));
+		System.out.println(datas.getTop());
+		System.out.println(datas.getBottom());
+		System.out.println(Arrays.toString(datas.getTop(2)));
+		System.out.println(Arrays.toString(datas.getBottom(3)));
+
+		datas.sort(true);
+
+		for (int i = 0; i < datas.length(); i++) {
+			System.out.print(datas.getName(i));
+			System.out.println(datas.getScore(i));
+		}
+
 	}
 }
