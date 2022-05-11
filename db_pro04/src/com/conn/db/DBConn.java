ffa662b5c7881d2aabf18acf929bd92734b1fd74
강승호 <kkang4913@gmail.com>
2022-05-11 16:51:22 +0900

    데이터베이스 연결 객체 정리

--- db_pro04/src/com/conn/db/DBConn.java
+++ db_pro04/src/com/conn/db/DBConn.java
@@ -9,62 +9,54 @@ import oracle.net.aso.m;
 
 public class DBConn {
 
-	public void localConnect() throws Exception {
-		//1. Driver 등록
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		
-		//2. DBMS 연결
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1","puser1","Kangsl4913112");
-		
-		//3. Statement 생성
-		Statement stat = conn.createStatement();
-		
-		//4. SQL 질의문 전송 및 반환
-		//int rowCount = stat.executeUpdate("INSERT INTO DEPARTMENTS VALUES(280, 'Tester', NULL, 1700)");
-		//int rowCount = stat.executeUpdate("UPDATE DEPARTMENTS SET DEPARTMENT_NAME = 'Dept Tester' WHERE DEPARTMENT_ID = 280");
-		int rowCount = stat.executeUpdate("DELETE FROM DEPARTMENTS WHERE DEPARTMENT_ID =280");
-		System.out.println(rowCount + "개 행이 반영되었습니다.");
-		
-		ResultSet rs = stat.executeQuery("SELECT * FROM DEPARTMENTS" );
-		while(rs.next()) {
-				System.out.print(rs.getString(1) + "\t");
-				System.out.print(rs.getString(2) + "\t");
-				System.out.print(rs.getString(3) + "\t");
-				System.out.print(rs.getString(4) + "\n");
-		}
-		
-		//5. 객체 반환
-		rs.close();
-		stat.close();
-		conn.close();
-	}
-	public void cloudConnect() throws Exception {
-		//1. Driver 등록
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		
-		//2. DBMS 연결
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@db202204211937_low?TNS_ADMIN=C:\\Users\\kang\\eclipse\\oracle\\Wallet_DB202204211937_2","rkdtmdgh","Tmdghrkd12345678");
-		
-		//3. Statement 생성
-		Statement stat = conn.createStatement();
-		
-		//4. SQL 질의문 전송 및 반환
-		ResultSet rs = stat.executeQuery("SELECT 'Hello Cloud' FROM DUAL" );
-		if(rs.next()) {
-			System.out.println(rs.getString(1));
-		}
-		
-		//5. 객체 반환
-		rs.close();
-		stat.close();
-		conn.close();
+	//공통된 부분의 기능 분리
+	private final static String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	private final static String BASE_URL = "jdbc:oracle:thin:@";
+	
+	private String url_address;
+	private Connection conn;
+	private Statement stat;
+	
+	
+	public DBConn(String address, String port, String serviceName, String username, String password) throws Exception {
+		url_address = String.format("%s:%s/%s", address, port, serviceName);
+		this.createConnection(username, password);
 	}
 	
-	public static void main(String[] args) throws Exception {
-		DBConn myDB = new DBConn();
-		myDB.localConnect();
-		myDB.cloudConnect();
+	public DBConn(String tnsAlias, String walletPath, String username, String password) throws Exception {
+		url_address = String.format("%S?TNS_ADMIN=%s", tnsAlias, walletPath);
+		this.createConnection(username, password);
+	}
+	
+	private void createConnection(String username, String password) throws Exception {
+		//1. Driver 등록
+		Class.forName(DRIVER_NAME);
 		
+		//2. DBMS 연결
+		conn = DriverManager.getConnection(BASE_URL + url_address, username, password);
+		
+		//3. Statement 생성
+		stat = conn.createStatement();
+	}
+	
+	//SQL 질의문 전송 및 반환
+	public ResultSet sendSelectQuery(String query) throws Exception {
+		return this.stat.executeQuery(query);
+	}
+	public int sendInsertQuery(String query) throws Exception {
+		return this.stat.executeUpdate(query);
+	}
+	public int sendUpdateQuery(String query) throws Exception {
+		return this.stat.executeUpdate(query);
+	}
+	public int sendDeletetQuery(String query) throws Exception {
+		return this.stat.executeUpdate(query);
+	}
+	
+	//객체반환
+	public void close() throws Exception {
+		this.stat.close();
+		this.conn.close();
 	}
 
 }
