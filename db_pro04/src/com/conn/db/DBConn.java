8347f77572cf59392ecb44cae64590c6477a8d8d
강승호 <kkang4913@gmail.com>
2022-05-12 18:14:33 +0900

    데이터베이스 연결 정보를 파일로 분리

--- db_pro04/src/com/conn/db/DBConn.java
+++ db_pro04/src/com/conn/db/DBConn.java
@@ -1,9 +1,16 @@
 package com.conn.db;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import oracle.net.aso.m;
 
@@ -17,6 +24,24 @@ public class DBConn {
 	private Connection conn;
 	private Statement stat;
 	
+	public DBConn(File config) throws Exception {
+			Map<String, String> map = new HashMap<String, String>();
+			BufferedReader br =  new BufferedReader(new FileReader(config));
+			
+			while (br.ready()) {
+				String[] kv = br.readLine().split("=");
+				map.put(kv[0].strip(), kv[1].strip());
+			}
+			if (map.get("host") != null) {
+				url_address = String.format("%s:%s/%s",map.get("host"),map.get("port"),map.get("service"));
+				
+			}else if (map.get("tns_alias") !=null) {
+				url_address = String.format("%S?TNS_ADMIN=%s",map.get("tns_alias"),map.get("wallet_path "));
+			}else {
+				System.out.println(config.getName() + "파일의 데이터베이스 연결 구성 정보가 잘못되었습니다.");
+			}
+			this.createConnection(map.get("username") ,map.get("password"));
+		}
 	
 	public DBConn(String address, String port, String serviceName, String username, String password) throws Exception {
 		url_address = String.format("%s:%s/%s", address, port, serviceName);
---
+++ db_pro04/src/com/join/dao/JoinDAO.java
@@ -0,0 +1,128 @@
+package com.join.dao;
+
+import java.io.File;
+import java.sql.ResultSet;
+
+import com.conn.db.DBConn;
+import com.join.vo.JoinVo;
+
+public class JoinDAO {
+	private DBConn db;
+	
+	public JoinDAO() {
+		//local / docker
+		try {
+			db = new DBConn(new File(System.getProperty("user.home") + "/oracle_db.conf"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//오라클 클라우드
+//	try {
+//		db = new DBConn(new File(System.getProperty("user.home") + "/oracle_db2.conf"));
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	}
+//}
+//	public JoinDAO() {
+//		try {
+//			db = new DBConn("localhost","1521","XEPDB1","puser1","Kangsl4913112");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	try {
+//		db = new DBConn("db202204211937_low","1521","rkdtmdgh","Tmdghrkd12345678");
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	}
+//}
+	public boolean add(JoinVo data) {
+		// 데이터 추가(회원가입)
+		String query = String.format("INSERT INTO accounts VALUES('%s', '%s', '%s', '%c', %d, SYSDATE)"
+					,data.getUserid()
+					,data.getUserpw()
+					,data.getUsername()
+					,data.getGender()
+					,data.getAge()
+				); 
+		
+		try {
+			int result = db.sendInsertQuery(query);
+			
+			if (result == 1) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean modify(JoinVo data) {
+		// 데이터 수정
+		String query = "" 
+				+ "UPDATE accounts "
+				+ "SET PW ='" + data.getUserpw() + "'"
+				+ "    ,NAME ='" +data.getUsername() + "'"
+				+ "	   ,GENDER ='" + data.getGender() + "'"
+				+ "    ,AGE = " + data.getAge()
+				+ "    WHERE ID = '" + data.getUserid() + "'";
+		try {
+			int rs = db.sendUpdateQuery(query);
+			
+			if (rs ==1) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+	public boolean remove(JoinVo data) {
+		// 데이터 삭제(탈퇴)
+		String query = "" 
+				+ "DELETE FROM accounts "
+				+ "    WHERE ID = '" + data.getUserid() + "'";
+		try {
+			int rs = db.sendDeletetQuery(query);
+			
+			if (rs ==1) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+	
+	public JoinVo get(String userid) {
+		// 단일 데이터 조회
+		String query = String.format("SELECT * FROM accounts WHERE ID = '%s'", userid); 
+	
+	try {
+		ResultSet rs = db.sendSelectQuery(query);
+		
+		if (rs.next()) {
+			JoinVo data = new JoinVo();
+			data.setUserid(rs.getString("ID"));
+			data.setUserpw(rs.getString("PW"));
+			data.setUsername(rs.getString("NAME"));
+			data.setGender(rs.getString("GENDER").charAt(0));
+			data.setAge(rs.getInt("AGE"));
+			data.setCreateDate(rs.getDate("CREATEDATE"));
+			return data;
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+}		
+	}
+	
+	
+
